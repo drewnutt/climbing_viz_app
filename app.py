e7a5dd551e4ea890d97b1081df142928b4274a48
Drew McNutt <anm329@pitt.edu>
2025-01-27 23:25:38 -0500

    Boulder Update
    Adds ability to view bouldering data. Also allows pulling directly from
    the the web and storage on the users computer.

--- app.py
+++ app.py
@@ -1,26 +1,33 @@
 #!/usr/bin/env python
-
+import os
+import requests
 import plotly.express as px
 import plotly.graph_objects as go
 import dash
-from dash import dcc, html, Input, Output
+from dash import dcc, html, Input, Output, State
 from datetime import date
 import pandas as pd
 import base64
 import io
 
-# constant for grade/code conversion
-GRADES = [{"value":1700, "label":"5.easy"}, {"value":1900, "label":"5.7"},
-          {"value":2200, "label":"5.8"}, {"value":2500, "label":"5.9"},
-          {"value":2800, "label":"5.10a"}, {"value":3100, "label":"5.10b"},
-          {"value":3400, "label":"5.10c"}, {"value":4500, "label":"5.10d"},
-          {"value":4800, "label":"5.11a"}, {"value":5100, "label":"5.11b"},
-          {"value":5400, "label":"5.11c"}, {"value":6500, "label":"5.11d"},
-          {"value":6800, "label":"5.12a"}, {"value":7100, "label":"5.12b"},
-          {"value":7400, "label":"5.12c"}, {"value":8500, "label":"5.12d"},
-          {"value":8800, "label":"5.13a"}, {"value":9100, "label":"5.13b"},
-          {"value":9400, "label":"5.13c"}, {"value":10400, "label":"5.13d"}]
 
+if os.getenv('HOST'):
+    del os.environ['HOST']
+
+# constant for grade/code conversion
+GRADES = {1700: "5.easy",1900: "5.7", 2200: "5.8",2500: "5.9",
+          2800: "5.10a",3100: "5.10b", 3400: "5.10c",4500: "5.10d",
+          4800: "5.11a",5100: "5.11b", 5400: "5.11c",6500: "5.11d",
+          6800: "5.12a",7100: "5.12b", 7400: "5.12c",8500: "5.12d",
+          8800: "5.13a",9100: "5.13b", 9400: "5.13c",10400: "5.13d"}
+BOULDER_GRADES = {20000 + 100 * i: f"V{i}" for i in range(1,18)} | \
+            {20000 + 100 * i - 30: f"V{i}-" for i in range(2,18)}  | {20000 + 100 * i + 10:f"V{i}+" for i in range(1,17)} | \
+            {20000 + 100 * i + 50: f"V{i}-{i+1}" for i in range(2,17)}  
+BOULDER_GRADES = BOULDER_GRADES | {20000: "V-easy", 20005: "V0-", 20010: "V0+", 20008: "V0"}
+# sort the boulder grades by value
+BOULDER_GRADES = dict(sorted(BOULDER_GRADES.items(), key=lambda x: x[0]))
+
+# generate BOULDER_GRADES 
 # Plotly configuration
 config = {'displayModeBar': False}
 
@@ -43,10 +50,23 @@ app.layout = html.Div([
                'textAlign': 'center',
                'margin': '10px'},
                multiple=False),
-    html.Div(children=['Route Filters'], style={'fontSize': '18px'}),
-    dcc.Dropdown(['Sport', 'Trad', 'TR'], ['Sport'],
-                multi=True, searchable=False, id='type-dropdown',
-                style={'width': '75vw'}),
+    html.Div(children=['Enter your Mountain Project profile URL to download your ticks.csv'],
+             style={'fontSize': '18px'}),
+    dcc.Input(id='mp-url', type='url', placeholder='https://www.mountainproject.com/user/USERNUM/USER-NAME',
+              style={'width': '75vw'}),
+    # add a button to download ticks.csv
+    html.Button('Download', id='download-button', n_clicks=0),
+    html.Div(children=[
+            html.Div(children=[html.H4(children='Route Type'), 
+                        dcc.RadioItems(['Rope','Boulder'],'Rope',id='route-type',
+                                style={'width':'75vw'})],
+                        style={'width':'75vw', 'flex-basis':'10rem'}),
+            html.Div(children=[html.H4(children='Rope Type'),
+                    dcc.Checklist(['Sport', 'Trad', 'TR'], ['Sport'],
+                                id='rope-type',style={'width': '75vw'})],
+                    style={'width':'75vw','flex-basis':'10rem'},id='rope-div'),
+            ], style={'display':'flex','flex-direction':'row'}),
+
     dcc.Dropdown(['Onsight', 'Flash', 'Redpoint', 'Pinkpoint', 'Fell/Hung', 'N/A'],
                  ['Onsight', 'Flash', 'Redpoint'],
                  multi=True, searchable=False, id='send-dropdown',
@@ -55,26 +75,38 @@ app.layout = html.Div([
                  max_date_allowed=date(2025, 1, 1), start_date=date(2022, 1, 1), end_date=date.today(),
                  id='date-range', style={'width':'75vw'}),
     html.Div(children=['Max Grade:',
-                       dcc.Dropdown(options=GRADES, value=8500, id='max-dropdown',
+        dcc.Dropdown(options=[{'label': value, 'value': key} for key,value in GRADES.items()], value=8500, id='max-dropdown',
                                     searchable=False, clearable=False)],
             style={'width':'30vw'}),
-    dcc.Checklist(['Include Multipitch Routes'],
-                  ['Include Multipitch Routes'],
-                  id='multi-filter'),
-    dcc.Graph(id='graph-pyramid', config=config,
-              style={'width':'80vw', 'height':'80vh'})
+    html.Div(children=[dcc.Checklist(['Include Multipitch Routes'],['Include Multipitch Routes'],
+                  id='multi-filter'),], id='multi-div', style={'width':'75vw'}),
+    # make a new Div that has a checklist for combining +,-,and normal boulder grades
+    html.Div(children=[dcc.Checklist(['Combine -,+, and normal grades'],['Combine -,+, and normal grades'],
+                  id='boulder-filter'),], id='boulder-div', hidden=True, style={'width':'75vw'}),
+    dcc.Loading([dcc.Graph(id='graph-pyramid', config=config,
+              style={'width':'80vw', 'height':'80vh'})]),
+    dcc.Store(id='ticks-data-store')
 ])
+@app.callback(
+        Output('max-dropdown', 'options'),
+        Output('max-dropdown', 'value'),
+        Output('multi-div', 'hidden'),
+        Output('boulder-div', 'hidden'),
+        Output('rope-div', 'hidden'),
+        Output('send-dropdown', 'options'),
+        [Input('route-type', 'value')],
+        )
+def update_route_type(route_type):
+    if route_type == 'Rope':
+        grades = [{'label': value, 'value': key} for key,value in GRADES.items()]
+        return grades, 8500, False, True, False, ['Onsight', 'Flash', 'Redpoint', 'Pinkpoint', 'Fell/Hung', 'N/A']
+    elif route_type == 'Boulder':
+        print(BOULDER_GRADES)
+        boulder_grades = [{'label': value, 'value': key} for key,value in BOULDER_GRADES.items()]
+        return boulder_grades, 20800, True, False, True, ['Send', 'Attempt','Flash']
 
-def parse_contents(contents, filename, criteria_type, criteria_send, start_date, end_date,
-                   criteria_max, criteria_multi):
-    _, content_string = contents.split(',')
-
-    # Decode the base64 string & Read CSV
-    decoded = io.StringIO(base64.b64decode(content_string).decode('utf-8'))
-    ticks = pd.read_csv(decoded, parse_dates=['Date'])
-
-    # Filter the dataframe based on criteria
-    ticks = ticks.loc[ticks['Route Type'].isin(criteria_type), :]
+def handle_roped(rope_type, ticks, criteria_send, start_date, end_date, criteria_max, criteria_multi):
+    ticks = ticks.loc[ticks['Route Type'].isin(rope_type), :]
     if start_date is not None:
         ticks = ticks.loc[ticks['Date'] >= pd.to_datetime(start_date), :]
     if end_date is not None:
@@ -85,13 +117,53 @@ def parse_contents(contents, filename, criteria_type, criteria_send, start_date,
         ticks = ticks.loc[ticks['Lead Style'].isin(criteria_send), :]
     if criteria_multi == []:
         ticks = ticks.loc[ticks['Pitches']==1, :]
-    ticks = ticks.loc[ticks['Rating Code'] <= criteria_max, :]
+    ticks = ticks.loc[ticks['Rating Code']<= criteria_max, :]
 
     # Calculate climbing pyramid metrics
     ticks['Grade'] = pd.cut(
         ticks['Rating Code'],
-        bins=[0] + [item['value'] for item in GRADES if item['value'] <= criteria_max],
-        labels=[item['label'] for item in GRADES if item['value'] <= criteria_max])
+        bins=[0] + [key for key in GRADES.keys() if key <= criteria_max],
+        labels=[value for key, value in GRADES.items() if key <= criteria_max])
+    return ticks
+
+def handle_boulders(ticks, criteria_send, start_date, end_date, criteria_max, criteria_boulder):
+    ticks = ticks.loc[ticks['Style'].isin(criteria_send)]
+    if start_date is not None:
+        ticks = ticks.loc[ticks['Date'] >= pd.to_datetime(start_date), :]
+    if end_date is not None:
+        ticks = ticks.loc[ticks['Date'] <= pd.to_datetime(end_date), :]
+    ticks = ticks.loc[ticks['Rating Code'] <= criteria_max, :]
+    ticks['Grade'] = pd.cut(
+        ticks['Rating Code'],
+        bins=[0] + [key for key in BOULDER_GRADES.keys() if key <= criteria_max],
+        labels=[value for key, value in BOULDER_GRADES.items() if key <= criteria_max])
+    if criteria_boulder:
+        ticks['Grade'] = ticks['Grade'].apply(lambda x: x.replace('-', '').replace('+', '') if x != 'V-easy' else x)
+
+    return ticks
+
+@app.callback(
+        Output('graph-pyramid', 'figure', allow_duplicate=True),
+        [Input('ticks-data-store', 'data'),
+         Input('route-type', "value"),
+         Input('rope-type', "value"),
+         Input('send-dropdown', "value"),
+         Input('date-range', "start_date"),
+         Input('date-range', "end_date"),
+         Input('max-dropdown', "value"),
+         Input('multi-filter', "value"),
+         Input('boulder-filter', "value"),],
+         prevent_initial_call=True)
+def parse_contents(ticks_data,route_type, rope_type, criteria_send, start_date, end_date,
+                   criteria_max, criteria_multi, criteria_boulder):
+    if ticks_data is None:
+        return go.Figure()
+    ticks = pd.read_json(io.StringIO(ticks_data), orient='split')
+    # Filter the dataframe based on criteria
+    if route_type == "Rope":
+        ticks = handle_roped(rope_type, ticks, criteria_send, start_date, end_date, criteria_max, criteria_multi)
+    else:
+        ticks = handle_boulders(ticks, criteria_send, start_date, end_date, criteria_max, criteria_boulder)
     counts = ticks.groupby('Grade', observed=False)['URL'].nunique().reset_index()
     counts.columns = ['Grade', 'Routes']
     counts.sort_values(by='Grade', ascending=False, inplace=True)
@@ -102,28 +174,29 @@ def parse_contents(contents, filename, criteria_type, criteria_send, start_date,
     return fig
 
 @app.callback(
-        Output('graph-pyramid', 'figure'),
-        [Input('upload-data', 'contents'),
-         Input('type-dropdown', "value"),
-         Input('send-dropdown', "value"),
-         Input('date-range', "start_date"),
-         Input('date-range', "end_date"),
-         Input('max-dropdown', "value"),
-         Input('multi-filter', "value")],
-         [dash.dependencies.State('upload-data', 'filename')])
-def update_output(contents, criteria_type, criteria_send, start_date, end_date,
-                 criteria_max, criteria_multi, filename):
-    if contents is None:
-        dummy = dict(
-            Routes=[1, 0, 1, 0, 2, 3, 5, 8, 16, 32, 64, 80, 70, 90, 100, 120],
-            Grade=["5.12d", "5.12c", "5.12b", "5.12a", "5.11d",
-                   "5.11c", "5.11b", "5.11a", "5.10d", "5.10c",
-                   "5.10b", "5.10a", "5.9", "5.8", "5.7", "5.easy"])
-        return px.funnel(dummy, x='Routes', y='Grade', title="(Demo Data) Route Pyramid")
-    else:
-        fig = parse_contents(contents, filename, criteria_type, criteria_send, start_date, end_date,
-                             criteria_max, criteria_multi)
-        return fig
+    Output('ticks-data-store', 'data', allow_duplicate=True),
+    [Input('upload-data', 'contents')],
+    prevent_initial_call=True)
+def upload_update_output(upload_data):
+    _, upload_data = upload_data.split(',')
+    decoded = io.StringIO(base64.b64decode(upload_data).decode('utf-8'))
+    downloaded_ticks = pd.read_csv(decoded, parse_dates=['Date'])
+    return downloaded_ticks.to_json(date_format='iso', orient='split')
+
+@app.callback(
+    Output('ticks-data-store', 'data', allow_duplicate=True),
+    [Input('download-button', 'n_clicks')],
+    [State('mp-url', 'value')],
+    prevent_initial_call=True
+)
+def mp_update_output(n_clicks, mp_url):
+    assert mp_url.startswith('https://www.mountainproject.com/user/'), "Invalid URL"
+    mp_url += '/tick-export'
+    page = requests.get(mp_url)
+    assert page.status_code == 200, f"Invalid URL: {mp_url}"
+    decoded = io.StringIO(page.text)
+    downloaded_ticks = pd.read_csv(decoded, parse_dates=['Date'])
+    return downloaded_ticks.to_json(date_format='iso', orient='split')
 
 if __name__ == '__main__':
     app.run_server(debug=True)
